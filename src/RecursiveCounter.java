59c87b0388948275988bb3edf2d6cce98a899739
unknown <BT_2N2_06@BKTECH.LOCAL>
2024-03-25 13:38:18 -0400

    Initial commit

---
+++ src/RecursiveCounter.java
@@ -0,0 +1,10 @@
+public class RecursiveCounter {
+    public static void main(String [] args) {
+        recursiveCounter(10);
+    }
+
+    public static void recursiveCounter(int number) {
+        // write this method
+    }
+}
+
